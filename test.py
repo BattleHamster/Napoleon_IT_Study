ecd75770728627d95911417c75b40488f7a625c8
Kristina Lubchuk <christy.lubchuk@gmail.com>
2020-11-25 21:51:22 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        result = 0
+
+        numbers = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400,
+                   "D": 500, "CM": 900, "M": 1000, }
+        i = 0
+        while i < len(s):
+            if i == len(s) - 1:
+                result += (numbers[s[i]])
+                i += 1
+            elif s[i] + s[i + 1] in numbers.keys():
+                result += (numbers[s[i] + s[i + 1]])
+                i += 2
+            else:
+                result += (numbers[s[i]])
+                i += 1
+
+        return result
